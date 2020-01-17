6b72174239a2503b7b4a24f8178b109a17c9b8be
pantianying <601666418@qq.com>
2020-01-17 10:22:13 +0800

    add code

---
+++ gotest/chan/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var ch1 chan *reqStu
+
+type reqStu struct {
+	done chan struct{}
+	data string
+}
+
+func main() {
+	ch1 = make(chan *reqStu, 100)
+	for {
+		time.Sleep(1 * time.Second)
+		req := &reqStu{
+			done: make(chan struct{}),
+			data: "data example",
+		}
+		ch1 <- req
+		select {
+		case <-req.done:
+			fmt.Println("做完了")
+		case <-time.After(3 * time.Second):
+			fmt.Println("超时了")
+		}
+	}
+}
+func work() {
+	for {
+		req := <-ch1
+		time.Sleep(1 * time.Second)
+		close(req.done) //代表做完了
+	}
+}
